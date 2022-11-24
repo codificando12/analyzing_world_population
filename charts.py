82910662097d3434c95b86b5bbd8fe38d6c05c48
Emilio <planeta.chompi18@gmail.com>
2022-11-25 07:24:20 +0800

    trying charts

--- charts.py
+++ charts.py
@@ -5,13 +5,15 @@ def generate_bar_chart(country, labels, values):
     fig, ax = plt.subplots()
     ax.bar(labels, values)
     plt.title(country)
-    plt.show()
+    plt.savefig(f'./img/{country}.png')
+    plt.close()
 
 def generate_pie_chart(labels, values):
 
     fig, ax = plt.subplots()
     ax.pie(values, labels=labels)
-    plt.show()
+    plt.savefig('pie.png')
+    plt.close()
 
 if __name__ == '__main__':
     countries = [
